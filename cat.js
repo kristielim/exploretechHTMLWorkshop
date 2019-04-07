c67ceba9e0b9422e20209d2ade26a989b76d759b
Kristie Lim <kristie.lim@gmail.com>
2019-04-06 17:45:49 -0700

    Six levels

--- cat.js
+++ cat.js
@@ -1,11 +1,36 @@
 let index = 0;
-let catIndex = 0;
-let removeCatIndex = 0;
 		
 function isCatSmall() {
 	const heightInPixels = $('#big-cat').css('height');
 	const height = parseInt(heightInPixels.slice(0, -2));
 	if (height < 300) {
+		return true;
+	}
+	return false;
+}
+
+function isTextBlack() {
+	const color = $('.blog-text').css('color');
+	console.log(color);
+	if (color === 'rgb(0, 0, 0)') {
+		return true;
+	}
+	return false;
+}
+
+function isTextCorrect() {
+	const text = $('#blog-text-pizza').text();
+	console.log(text);
+	if (text === 'Eating some pizza') {
+		return true;
+	}
+	return false;
+}
+
+function isTextItalics() {
+	const property = $('.blog-text').css('font-style');
+	console.log(property);
+	if (property === 'italic') {
 		return true;
 	}
 	return false;
@@ -19,6 +44,15 @@ function areCatsNotDizzy() {
 	return false;
 }
 
+function areCatsReunited() {
+	const property = $('.fam-cat').css('margin');
+	console.log(property);
+	if (property === '0px') {
+		return true;
+	}
+	return false;
+}
+
 const steps = [
 	{
 		instruction: 'Whoa! A giant cat appeared! Make its height smaller.',
@@ -26,67 +60,57 @@ const steps = [
 		failMessage: 'Cat is still too big.'
 	},
 	{
+		instruction: 'The cat is starting a blog, but the pink text is a little hard to read. Change it to black!',
+		isCompleted: isTextBlack,
+		failMessage: 'Text is not black.'
+	},
+	{
+		instruction: 'Oh no! Fix the typo!',
+		isCompleted: isTextCorrect,
+		failMessage: 'Still misspelled :('
+	},
+	{
+		instruction: 'The cat is leaning over...whoosh. Make the text match by changing the font-style to italic!',
+		isCompleted: isTextItalics,
+		failMessage: 'Text is not italics.'
+	},
+	{
 		instruction: `These small cats are so dizzy. 
 						Help them out by changing their animation to jump.`,
 		isCompleted: areCatsNotDizzy,
 		failMessage: `Oops they're still dizzy.`
+	},
+	{
+		instruction: 'Reunite the family of cats by making the margin 0 pixels.',
+		isCompleted: areCatsReunited,
+		failMessage: 'Still far apart, so sad.'
 	}
 ];
 
 function nextStep() {
-	console.log('nextStep');
+	// hide current step
+	$(`#step-${index}`).hide();
+
 	index++;
 	if (index >= steps.length) {
-		alert('completed!');
+		$('#popup').hide();
+		$('#last-step').css('display', 'flex');
 		return;
 	}
+
+	// show next step
+	$(`#step-${index}`).css('display', 'flex');
+
+	// update instructions
 	$('#instructions').text(steps[index].instruction);
-}
-
-
-
-function spawnCat(){
-	// pick next cat in cat index (mod by number of cats)
-	// set position to position of black hole
-	// fade in
-	// spiral out
-
-	const spawnInterval = Math.random() * 5000;
-	const fadeOutInterval = 0.75 * spawnInterval;
-
-	// add a cat to the black hole
-	const catHTML = `<img id="omg-cat-${catIndex}"
-						  class="omg-cat" 
-						  src="https://thumbs.gfycat.com/WellmadeOddAmericanbobtail-max-1mb.gif"/>`;
-	$('#black-hole').append(catHTML);
-
-	// cat moves outward
-	// $(`#omg-cat-${catIndex}`).click(function() {
-	// 	console.log(`animating cat ${catIndex}`); 
-	// 	$(`#omg-cat-${catIndex}`).animate({
-	// 		left: '+=100px'
-	// 	}, 'fast');
-	// });
-	// $(`#omg-cat-${catIndex}`).animate({
-	// 	left: '+=1000px'
-	// }, 'slow');
-	$(`#omg-cat-${catIndex}`).spiral({InsideOut: 1, Easing:4, Duration: fadeOutInterval, Radius: 200});
-
-	// fade out and remove cat when done
-	$(`#omg-cat-${catIndex}`).fadeOut(fadeOutInterval, function() {
-		$(`#omg-cat-${removeCatIndex}`).remove();
-		removeCatIndex++;
-	});
-
-	catIndex++;
-
-	// spawn another cat
-    setTimeout(spawnCat, spawnInterval);
 }
 
 $(window).on('load', function () {
 	// update instructions
 	$('#instructions').text(steps[index].instruction);
+
+	// show first step
+	$(`#step-${index}`).css('display', 'flex');
 
 	// move to next step when next button is clicked
     $('#next').click(function () {
@@ -98,7 +122,5 @@ $(window).on('load', function () {
     		alert(currStep.failMessage);
     	}
     });
-
-    spawnCat();
 
 });
